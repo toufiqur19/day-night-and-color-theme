fc4c41bdf84a50cef5bd383d8e315dfcef6a859a
Toufiqur Rahman Sobuj <100756958+toufiqur19@users.noreply.github.com>
2023-10-04 18:07:31 +0600

    Add files via upload

---
+++ day-night&color_theme/script.js
@@ -0,0 +1,49 @@
+/*============================================= 
+       day/night Control Theme start 
+=============================================*/
+const icons = [...document.querySelectorAll(".theme-icon")];
+
+icons.forEach((icon) => {
+  if (icon) {
+    icon.addEventListener("click", () => {
+      document.body.classList.toggle("dark");
+
+      if (document.body.classList.contains("dark")) {
+        icon.innerHTML = '<i class="bx bx-sun"></i>';
+        icon.style.color = "white";
+      } else {
+        icon.innerHTML = '<i class="bx bx-moon"></i>';
+        icon.style.color = "#121713";
+      }
+    });
+  }
+});
+/*============================================= 
+       day/night Control Theme end 
+=============================================*/
+
+
+/*============================================= 
+         Color Control start
+=============================================*/
+const widget = document.querySelector(".widget");
+const control = document.querySelector(".control");
+
+widget.addEventListener("click", () => {
+  control.classList.toggle("open");
+});
+
+const colors = [...document.querySelectorAll(".colors span")];
+document.querySelector(":root").style.setProperty("--customColor", "#42caff");
+
+colors.forEach((color) => {
+  color.addEventListener("click", () => {
+    const currentColor = color.dataset.id;
+    document
+      .querySelector(":root")
+      .style.setProperty("--customColor", currentColor);
+  });
+});
+/*============================================= 
+         Color Control end
+=============================================*/
